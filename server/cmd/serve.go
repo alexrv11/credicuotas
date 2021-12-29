fdf6b947315a52461be27d610edbd525b40b5ddf
Alex Ventura <alex.rv11@gmail.com>
2021-12-29 19:22:52 -0400

    change footer labels

--- server/cmd/serve.go
+++ server/cmd/serve.go
@@ -55,7 +55,6 @@ var serveCmd = &cobra.Command{
 		router := chi.NewRouter()
 
 		router.Use(cors.Handler(cors.Options{
-			// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
 			AllowedOrigins:   []string{"https://*", "http://*"},
 			// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
 			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
