2d4a5ed029875c0fcdaa00c904f4ab43ab7ee0af
Alex Ventura <alex.rv11@gmail.com>
2022-04-01 08:34:12 -0400

    change api url

--- client/admin/src/api/client.js
+++ client/admin/src/api/client.js
@@ -2,8 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    //uri: 'https://api-4huvpq5kca-uc.a.run.app/graphql'
-    uri: 'http://localhost:8282/graphql'
+    uri: 'https://api-4huvpq5kca-uc.a.run.app/graphql'
 });
 
 const authLink = setContext((_, { headers }) => {
